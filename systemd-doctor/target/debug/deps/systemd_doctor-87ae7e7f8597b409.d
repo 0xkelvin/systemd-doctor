b738d1e4eb576a9e1d1503a02a57d1418bf82718
Kelvin <quocvm1@gmail.com>
2024-06-17 21:25:16 +0800

    i make stupid on git ignore

--- systemd-doctor/target/debug/deps/systemd_doctor-87ae7e7f8597b409.d
+++
@@ -1,5 +0,0 @@
-/Users/kelvin/KelvinZone/systemd-doctor/systemd-doctor/target/debug/deps/libsystemd_doctor-87ae7e7f8597b409.rmeta: src/main.rs
-
-/Users/kelvin/KelvinZone/systemd-doctor/systemd-doctor/target/debug/deps/systemd_doctor-87ae7e7f8597b409.d: src/main.rs
-
-src/main.rs:
--- systemd-doctor/target/debug/deps/systemd_doctor-9cec394e66d5b6bf.d
+++
@@ -1,5 +0,0 @@
-/Users/kelvin/KelvinZone/systemd-doctor/systemd-doctor/target/debug/deps/libsystemd_doctor-9cec394e66d5b6bf.rmeta: src/main.rs
-
-/Users/kelvin/KelvinZone/systemd-doctor/systemd-doctor/target/debug/deps/systemd_doctor-9cec394e66d5b6bf.d: src/main.rs
-
-src/main.rs:
