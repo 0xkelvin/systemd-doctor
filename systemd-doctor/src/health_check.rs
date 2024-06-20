92d957384b7c296daa6182a9633b6eb67bbcdba7
Kelvin <quocvm1@gmail.com>
2024-06-20 21:33:49 +0800

    apply oop for health_check

--- systemd-doctor/src/health_check.rs
+++ systemd-doctor/src/health_check.rs
@@ -1,32 +1,65 @@
 use std::{os::unix::process, thread, time::Duration};
-
 use sysinfo::System;
 
-pub fn check_cpu_load(service_name: &str, _threshold: Option<f32>) -> Result<f32, String> {
-    let mut system = System::new_all();
-    system.refresh_all();
-    thread::sleep(Duration::from_millis(500));
-    system.refresh_all();
-    for process in system.processes_by_exact_name(service_name) {
-        // println!("{}: {}", process.name(), process.cpu_usage());
-        return Ok(process.cpu_usage());
+pub struct HealthCheck {
+    system: System,
+}
+
+impl HealthCheck {
+    pub fn new() -> Self {
+        let mut system = System::new_all();
+        system.refresh_all();
+        Self { system }
     }
-    Err(format!("Service {} not found or CPU usage", service_name))
+
+    pub fn check_cpu_load(
+        &mut self,
+        service_name: &str,
+        _threshold: Option<f32>,
+    ) -> Result<f32, String> {
+        self.system.refresh_all();
+        thread::sleep(Duration::from_millis(500));
+        self.system.refresh_all();
+        for process in self.system.processes_by_exact_name(service_name) {
+            println!("{}: cpu_load: {}", process.name(), process.cpu_usage());
+            return Ok(process.cpu_usage());
+        }
+        Err(format!("Service {} not found or CPU usage", service_name))
+    }
+
+    pub fn check_memory_usage(
+        &mut self,
+        service_name: &str,
+        _threshold: Option<u64>,
+    ) -> Result<u64, String> {
+        self.system.refresh_all();
+        thread::sleep(Duration::from_millis(500));
+        self.system.refresh_all();
+        for process in self.system.processes_by_exact_name(service_name) {
+            println!("{}: memory: {}", process.name(), process.memory());
+            return Ok(process.memory());
+        }
+        Err(format!("Service {} not found or CPU usage", service_name))
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::health_check::HealthCheck;
+
     #[test]
     fn test_check_cpu_load_on_valid_service() {
+        let mut health_check = HealthCheck::new();
         let service_name = "bash";
-        let cpu_load = super::check_cpu_load(service_name, None);
+        let cpu_load = health_check.check_cpu_load(service_name, None);
         assert!(cpu_load.is_ok());
     }
 
     #[test]
     fn test_check_cpu_load_on_invalid_service() {
+        let mut health_check = HealthCheck::new();
         let service_name = "invalid_service";
-        let cpu_load = super::check_cpu_load(service_name, None);
+        let cpu_load = health_check.check_cpu_load(service_name, None);
         assert!(cpu_load.is_err());
     }
 }
--- systemd-doctor/src/main.rs
+++ systemd-doctor/src/main.rs
@@ -5,4 +5,13 @@ mod health_check;
 
 fn main() {
     println!("Hello, world!");
+    loop {
+        let mut health = health_check::HealthCheck::new();
+        let service_name = "service_test_2";
+        let cpu_load = health.check_cpu_load(service_name, None);
+        let memory_usage = health.check_memory_usage(service_name, None);
+        // println!("CPU load: {:?}", cpu_load);
+        // println!("Memory usage: {:?}", memory_usage);
+        thread::sleep(Duration::from_secs(1));
+    }
 }
