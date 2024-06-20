ae2ed4b62713286355f7505016bc6f6823896edb
Kelvin <quocvm1@gmail.com>
2024-06-20 18:50:10 +0800

    try to use sysinfo instead of linux command

--- systemd-doctor/src/health_check.rs
+++ systemd-doctor/src/health_check.rs
@@ -1,34 +1,32 @@
-use std::process::Command;
+use std::{os::unix::process, thread, time::Duration};
 
-/* 
-sh -c "ps -C <service name> -o %cpu= | awk '{s+=\$1} END {print s}'"
-*/
-// not implement the threshold yet
-pub fn check_cpu_load(service: &str, _threshold: Option<f32>) -> Result<f32, String> {
-    let output = Command::new("sh")
-    .arg("-c")
-    .arg(format!("ps -C {} -o %cpu= | awk '{{s+=$1}} END {{print s}}'", service))
-    .output()
-    .expect("Failed to execute command");
+use sysinfo::System;
 
-let load: f32 = String::from_utf8_lossy(&output.stdout)
-    .trim()
-    .parse()
-    .unwrap_or(0.0);
-    Ok(load)
-    
+pub fn check_cpu_load(service_name: &str, _threshold: Option<f32>) -> Result<f32, String> {
+    let mut system = System::new_all();
+    system.refresh_all();
+    thread::sleep(Duration::from_millis(500));
+    system.refresh_all();
+    for process in system.processes_by_exact_name(service_name) {
+        // println!("{}: {}", process.name(), process.cpu_usage());
+        return Ok(process.cpu_usage());
+    }
+    Err(format!("Service {} not found or CPU usage", service_name))
 }
-
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    #[test]
+    fn test_check_cpu_load_on_valid_service() {
+        let service_name = "bash";
+        let cpu_load = super::check_cpu_load(service_name, None);
+        assert!(cpu_load.is_ok());
+    }
 
     #[test]
-    fn test_check_cpu_load() {
-        let service = "service_test_2";
-        let cpu_load = check_cpu_load(service, None);
-        assert!(cpu_load.is_ok());
-        println!("Current CPU load: {:.2}%", cpu_load.unwrap());
+    fn test_check_cpu_load_on_invalid_service() {
+        let service_name = "invalid_service";
+        let cpu_load = super::check_cpu_load(service_name, None);
+        assert!(cpu_load.is_err());
     }
 }
