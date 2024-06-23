e614c58b9e0669f55bfbb28cba4193adb7c9a7a9
Kelvin <quocvm1@gmail.com>
2024-06-23 21:30:02 +0800

    adding journal log idea, to get log for a service

---
+++ systemd-doctor/src/journal_log.rs
@@ -0,0 +1,54 @@
+use std::process::Command;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::thread;
+use std::time::{Duration, SystemTime};
+use std::io::Result;
+
+pub fn extract_service_logs(service: &str, since: &str) -> Result<String> {
+    let output = Command::new("journalctl")
+        .arg("-u")
+        .arg(service)
+        .arg("--since")
+        .arg(since)
+        .arg("--no-pager")
+        .output()?;
+    if !output.status.success() {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        return Err(std::io::Error::new(std::io::ErrorKind::Other, stderr))
+    }
+    Ok(String::from_utf8_lossy(&output.stdout).into_owned())
+}
+
+
+pub fn spawn_log_writer(service: &str, log_file: &str) -> Result<()> {
+    let log_file_path = log_file.to_string();
+    let service = service.to_string();
+    thread::spawn(move || {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(&log_file_path)
+            .expect("Failed to open log file");
+        let mut last_fetch_time = SystemTime::now();
+        loop {
+            let since = format!("{:?}", last_fetch_time);
+            match extract_service_logs(&service, &since) {
+                Ok(logs) => {
+                    if !logs.trim().is_empty() {
+                        if let Err(e) = writeln!(file, "{}", logs) {
+                            eprintln!("Failed to write logs to file: {}", e);
+                        }
+                        last_fetch_time = SystemTime::now();
+                    }
+                }
+                Err(e) => eprintln!("Failed to fetch logs: {} {}", e, service),
+
+            }
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    Ok(())
+    
+}
--- systemd-doctor/src/main.rs
+++ systemd-doctor/src/main.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 
 mod cmd_health_check;
 mod sys_health_check;
+mod journal_log;
 
 fn main() {
     println!("Hello, world!");
@@ -12,6 +13,8 @@ fn main() {
         let service_name = "service_test_2";
         let cpu_load = cmd_health.cmd_check_cpu_load(service_name, None);
         let memory_usage = cmd_health.cmd_check_memory_usage_kb(service_name, 0);
+        let _start_journal = journal_log::spawn_log_writer(service_name, "service_test_2.log");
+
 
         // let cpu_load = health.check_cpu_load(service_name, None);
         // let memory_usage = health.check_memory_usage(service_name, None);
