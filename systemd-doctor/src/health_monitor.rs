298fc0e80aff25ab5c0b137ccd7b7c9c3af77bed
Kelvin <quocvm1@gmail.com>
2024-06-29 00:42:46 +0800

    tracking service and log information

---
+++ systemd-doctor/src/health_monitor.rs
@@ -0,0 +1,47 @@
+use crate::LogWriter;
+use std::thread;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+
+
+pub struct HealthMonitor {
+    services: Vec<String>,
+    check_interval: Duration, 
+    log_writer: LogWriter,
+}
+
+impl HealthMonitor {
+    pub fn new(service: Vec<String>, check_interval: Duration, log_file: &str, config_file: &str) -> Self {
+        let log_writer = LogWriter::new(log_file, config_file);
+
+        Self {
+            services: service,
+            check_interval,
+            log_writer,
+        }
+    }
+
+    pub fn start(&self) {
+        for service in &self.service {
+            let service_name = service.clone();
+            let log_writer = self.log_writer.clone();
+            thread::spawn(move || {
+                let mut last_fetch_time = SystemTime::now();
+                loop {
+                    since = format!("{:?}", last_fetch_time);
+                    match LogWriter::extract_service_logs(&service_name, &since) {
+                        Ok(logs) => {
+                            if let Err(e) = log_writer.write_log(&logs) {
+                                eprintln!("Failed to write logs: {}", e);
+                            }
+                            last_fetch_time = SystemTime::now();
+                        }
+                        Err(e) => eprintln!("Failed to fetch logs: {} {}", e, service_name),
+                    }
+                    thread::sleep(self.check_interval);
+                }
+
+            });
+        }
+    }
+}
